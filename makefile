5cde0af6f7a3432a6b322baf2935fb55b8cd61df
Hadarfro <122720878+Hadarfro@users.noreply.github.com>
2024-05-23 21:12:59 +0300

    fixing q2

--- makefile
+++ makefile
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 CXX = clang++
 CXXFLAGS = -std=c++11 -Werror -Wsign-conversion -g
 VALGRIND_FLAGS = -v --leak-check=full --show-leak-kinds=all --error-exitcode=99
@@ -26,28 +25,3 @@ valgrind: $(EXECUTABLES)
 
 clean:
 	rm -f $(EXECUTABLES) $(OBJECTS)
-=======
-# CXX = clang++
-# CXXFLAGS = -std=c++11 -Werror -Wsign-conversion -g
-# VALGRIND_FLAGS = -v --leak-check=full --show-leak-kinds=all --error-exitcode=99
-
-# SOURCES = mync.cpp 
-# OBJECTS = $(SOURCES:.cpp=.o)
-# EXECUTABLE = mync
-
-# .PHONY: all clean valgrind
-
-# all: $(EXECUTABLE)
-
-# $(EXECUTABLE): $(OBJECTS)
-# 	$(CXX) $(CXXFLAGS) -o $@ $^
-
-# %.o: %.c
-# 	$(CXX) $(CXXFLAGS) -c -o $@ $<
-
-# valgrind: $(EXECUTABLE)
-# 	valgrind $(VALGRIND_FLAGS) ./$(EXECUTABLE)
-
-# clean:
-# 	rm -f $(EXECUTABLE) $(OBJECTS)
->>>>>>> e12ebf9e6fbc378695912424a474562fc7edde01
--- mync.cpp
+++ mync.cpp
@@ -2,7 +2,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <unistd.h>
 using namespace std;
 
 #define MAX_COMMAND_LENGTH 1000
@@ -12,21 +11,20 @@ int main(int argc, char *argv[]) {
         cout << "Usage: " << argv[0] << " -e <command>" << endl;
         return 1;
     }
-    pid_t pid = fork();
-
-    if (pid == 0) {
-        // זהו התהליך הילד
-        execlp("ttt", "ttt", "123456789", nullptr);
-        // אם הגענו לכאן, execlp נכשל
-        std::cerr << "Failed to execute ttt\n";
-        return 1;
-    } else if (pid > 0) {
-        // זהו התהליך האב
-        wait(nullptr); // מחכה לסיום התהליך הילד
-    } else {
-        std::cerr << "Failed to fork\n";
+    char command[MAX_COMMAND_LENGTH];
+    strcpy(command, argv[2]); // Copy the command to execute
+    FILE *fp;
+    fp = popen(command, "r"); // Open a pipe to execute the command
+    if (fp == nullptr) {
+        std::cout << "Error executing command." << std::endl;
         return 1;
     }
-    cout << "test" << endl;
+    char buffer[MAX_COMMAND_LENGTH];
+    while (fgets(buffer, MAX_COMMAND_LENGTH, fp) != nullptr) {
+        cout << buffer; // Print output from the executed command
+    }
+    
+    pclose(fp); // Close the pipe
+    
     return 0;
 }
